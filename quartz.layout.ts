b1b68998f62189e2850c84ac5fe6daba1c817fd4
handanhome <liukjx@qq.com>
2026-07-20 00:19:48 +0800

    fix: sort explorer by file slug

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,17 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { FileTrieNode } from "./quartz/util/fileTrie"
+
+const explorerSort = (a: FileTrieNode, b: FileTrieNode) => {
+  if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+    return a.slugSegment.localeCompare(b.slugSegment, undefined, {
+      numeric: true,
+      sensitivity: "base",
+    })
+  }
+
+  return !a.isFolder && b.isFolder ? 1 : -1
+}
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -26,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({ sortFn: explorerSort })),
   ],
   right: [
     Component.Graph(),
@@ -43,7 +55,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({ sortFn: explorerSort })),
   ],
   right: [],
 }
